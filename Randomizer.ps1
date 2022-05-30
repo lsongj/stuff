69e06d6500540a107ae58c17638c41c1a8540e70
lsongj <106043157+lsongj@users.noreply.github.com>
2022-05-29 23:01:36 -0400

    Add files via upload

--- Randomizer.ps1
+++ Randomizer.ps1
@@ -6,14 +6,14 @@
 
 $Form_Picker = New-Object System.Windows.Forms.Form
     $Form_Picker.Text = "Challenge Randomizer"
-    $Form_Picker.Size = New-Object System.Drawing.Size(450,300)
+    $Form_Picker.Size = New-Object System.Drawing.Size(500,400)
     $Form_Picker.FormBorderStyle = "FixedDialog"
     $Form_Picker.TopMost = $true
     $Form_Picker.MaximizeBox = $false
     $Form_Picker.MinimizeBox = $false
     $Form_Picker.StartPosition = "CenterScreen"
     $Form_Picker.Font = "Segoe UI"
-
+	#You can add your own Image for the background by hardcoding the filepath below
     $objBackgroundImage = [system.drawing.image]::FromFile("C:\temp\randomizerFiles\background.PNG")
     $Form_Picker.BackgroundImage = $objBackgroundImage
     $Form_Picker.BackgroundImageLayout = "Stretch"
@@ -29,8 +29,8 @@ $label_Picker = New-Object System.Windows.Forms.Label
     $label_Picker.TextAlign = "MiddleCenter"
     $label_Picker.ForeColor = "White"
     $label_Picker.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("White"))
-    $label_Picker.Font = New-Object System.Drawing.Font("Bazooka",14,[System.Drawing.FontStyle]::Regular)
-    $label_Picker.Text = "TEST YOUR FATE"
+    $label_Picker.Font = New-Object System.Drawing.Font("Arial",14,[System.Drawing.FontStyle]::Bold)
+    $label_Picker.Text = "Test Your Worthiness"
     $Form_Picker.Controls.Add($label_Picker)
 
 #----------------------------------------------------------------------------------------------------------------
@@ -39,19 +39,22 @@ $label_Picker = New-Object System.Windows.Forms.Label
 
  $label_PickField = New-Object System.Windows.Forms.Label
     $label_PickField.Location = New-Object System.Drawing.Size(8,50)
-    $label_PickField.Size = New-Object System.Drawing.Size(50,32)
+    $label_PickField.Size = New-Object System.Drawing.Size(65,50)
     $label_PickField.TextAlign = "MiddleCenter"
     $label_PickField.ForeColor = "White"
     $label_PickField.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("White"))
+	$label_PickField.Font = New-Object System.Drawing.Font("Ariel",12,[System.Drawing.FontStyle]::Bold)
     $label_PickField.Text = "PICK"
     $Form_Picker.Controls.Add($label_PickField)
 
+#PICK LABEL TEXT BOX
   $label_PickRandom = New-Object System.Windows.Forms.Label
-    $label_PickRandom.Location = New-Object System.Drawing.Size(70,50)
-    $label_PickRandom.Size = New-Object System.Drawing.Size(150,32)
+    $label_PickRandom.Location = New-Object System.Drawing.Size(90,50)
+    $label_PickRandom.Size = New-Object System.Drawing.Size(250,50)
     $label_PickRandom.TextAlign = "MiddleCenter"
     $label_PickRandom.ForeColor = "White"
     $label_PickRandom.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("White"))
+	$label_PickRandom.Font = New-Object System.Drawing.Font("Arial",14,[System.Drawing.FontStyle]::Bold)
     $label_PickRandom.Text = ""
     $Form_Picker.Controls.Add($label_PickRandom)
 
@@ -60,20 +63,23 @@ $label_Picker = New-Object System.Windows.Forms.Label
 #BAN LABEL
 
 $label_BanField = New-Object System.Windows.Forms.Label
-    $label_BanField.Location = New-Object System.Drawing.Size(8,100)
-    $label_BanField.Size = New-Object System.Drawing.Size(50,32)
+    $label_BanField.Location = New-Object System.Drawing.Size(8,120)
+    $label_BanField.Size = New-Object System.Drawing.Size(65,50)
     $label_BanField.TextAlign = "MiddleCenter"
     $label_BanField.ForeColor = "White"
     $label_BanField.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("White"))
+	$label_BanField.Font = New-Object System.Drawing.Font("Ariel",12,[System.Drawing.FontStyle]::Bold)
     $label_BanField.Text = "BAN"
     $Form_Picker.Controls.Add($label_BanField)
 
+#BAN LABEL TEXT BOX
 $label_BanRandom = New-Object System.Windows.Forms.Label
-    $label_BanRandom.Location = New-Object System.Drawing.Size(70,100)
-    $label_BanRandom.Size = New-Object System.Drawing.Size(150,32)
+    $label_BanRandom.Location = New-Object System.Drawing.Size(90,120)
+    $label_BanRandom.Size = New-Object System.Drawing.Size(250,50)
     $label_BanRandom.TextAlign = "MiddleCenter"
     $label_BanRandom.ForeColor = "White"
     $label_BanRandom.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("White"))
+	$label_BanRandom.Font = New-Object System.Drawing.Font("Arial",14,[System.Drawing.FontStyle]::Bold)
     $label_BanRandom.Text = ""
     $Form_Picker.Controls.Add($label_BanRandom)
 
@@ -82,64 +88,115 @@ $label_BanRandom = New-Object System.Windows.Forms.Label
 #Modifier Check Box
 
 $objTypeCheckbox = New-Object System.Windows.Forms.Checkbox 
-$objTypeCheckbox.Location = New-Object System.Drawing.Size(8,205) 
-$objTypeCheckbox.Size = New-Object System.Drawing.Size(125,20)
-$objTypeCheckbox.TextAlign = "MiddleCenter"
-$objTypeCheckbox.Text = "Modifier Chance?"
-$objTypeCheckbox.TabIndex = 4
-$Form_Picker.Controls.Add($objTypeCheckbox)
+	$objTypeCheckbox.Location = New-Object System.Drawing.Size(8,295)
+	$objTypeCheckbox.Size = New-Object System.Drawing.Size(150,30)
+	$objTypeCheckbox.Font = New-Object System.Drawing.Font("Ariel",10,[System.Drawing.FontStyle]::Bold)
+	$objTypeCheckbox.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("White"))
+	$objTypeCheckbox.ForeColor = "White"
+	$objTypeCheckbox.TextAlign = "MiddleCenter"
+	$objTypeCheckbox.Text = "Include Modifier"
+	$objTypeCheckbox.TabIndex = 4
+	$Form_Picker.Controls.Add($objTypeCheckbox)
 
 #----------------------------------------------------------------------------------------------------------------
 
+#Modifier Chance Drop Box
 
-#Modifier Label
+$ChanceDropDownBox = New-Object System.Windows.Forms.ComboBox
+$ChanceDropDownBox.Location = New-Object System.Drawing.Size(8,325)
+$ChanceDropDownBox.Size = New-Object System.Drawing.Size(150,50)
+$ChanceDropDownBox.DropDownHeight = 150
+$Form_Picker.Controls.Add($ChanceDropDownBox)
+
+$modifierChanceArray = @(
+	'25%',
+	'50%',
+	'100%'
+)
+
+#populates the drop down menu from the above array
+Foreach($modifierChancePick in $modifierChanceArray){
+	$ChanceDropDownBox.Items.Add($modifierChancePick)
+}
+
+#----------------------------------------------------------------------------------------------------------------
+
+#MODIFIER LABEL
 
 $label_modifier = New-Object System.Windows.Forms.Label
-    $label_modifier.Location = New-Object System.Drawing.Size(8,150)
-    $label_modifier.Size = New-Object System.Drawing.Size(50,32)
+    $label_modifier.Location = New-Object System.Drawing.Size(8,190)
+    $label_modifier.Size = New-Object System.Drawing.Size(65,50)
     $label_modifier.TextAlign = "MiddleCenter"
     $label_modifier.ForeColor = "White"
     $label_modifier.BackColor = [System.Drawing.Color]::FromArgb(0,[System.Drawing.Color]::FromName("White"))
     $label_modifier.Text = ""
     $Form_Picker.Controls.Add($label_modifier)
 
+#MODIFIER LABEL CHECKBOX
 $label_ModifierRandom = New-Object System.Windows.Forms.Label
-    $label_ModifierRandom.Location = New-Object System.Drawing.Size(70,150)
-    $label_ModifierRandom.Size = New-Object System.Drawing.Size(150,32)
+    $label_ModifierRandom.Location = New-Object System.Drawing.Size(90,190)
+    $label_ModifierRandom.Size = New-Object System.Drawing.Size(250,50)
     $label_ModifierRandom.TextAlign = "MiddleCenter"
     $label_ModifierRandom.ForeColor = "White"
     $label_ModifierRandom.BackColor = [System.Drawing.Color]::FromArgb(0,[System.Drawing.Color]::FromName("White"))
+	$label_ModifierRandom.Font = New-Object System.Drawing.Font("Ariel",10,[System.Drawing.FontStyle]::Bold)
     $label_ModifierRandom.Text = ""
     $Form_Picker.Controls.Add($label_ModifierRandom)
 
 #----------------------------------------------------------------------------------------------------------------
 
+#No Regions Check Box
+
+$objTypeRegionCheckbox = New-Object System.Windows.Forms.Checkbox  
+	$objTypeRegionCheckbox.Location = New-Object System.Drawing.Size(8,260) 
+	$objTypeRegionCheckbox.Size = New-Object System.Drawing.Size(150,30)
+	$objTypeRegionCheckbox.Font = New-Object System.Drawing.Font("Ariel",10,[System.Drawing.FontStyle]::Bold)
+	$objTypeRegionCheckbox.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("White"))
+	$objTypeRegionCheckbox.ForeColor = "White"
+	$objTypeRegionCheckbox.TextAlign = "MiddleCenter"
+	$objTypeRegionCheckbox.Text = "Include Regions"
+	$objTypeRegionCheckbox.TabIndex = 4
+	$Form_Picker.Controls.Add($objTypeRegionCheckbox)
+
+
+#----------------------------------------------------------------------------------------------------------------
+
+
 
 #BUTTON
 
 $button_ClickMe = New-Object System.Windows.Forms.Button
-    $button_ClickMe.Location = New-Object System.Drawing.Size(150,200)
-    $button_ClickMe.Size = New-Object System.Drawing.Size(150,32)
-    $button_ClickMe.TextAlign = "MiddleCenter"
-    $button_ClickMe.Text = "Hold on tight..."
-    $button_ClickMe.Add_Click({$button_ClickMe.Text = "Calculating..."
 
-    $button_ClickMe.Enabled = $false
+	$button_ClickMe.Location = New-Object System.Drawing.Size(250,275)
+	$button_ClickMe.Size = New-Object System.Drawing.Size(200,50)
+	$button_ClickMe.Font = New-Object System.Drawing.Font("Ariel",12,[System.Drawing.FontStyle]::Regular)
+	$button_ClickMe.TextAlign = "MiddleCenter"
+	$button_ClickMe.Text = "It's go time"
+	$button_ClickMe.Add_Click({$button_ClickMe.Text = "Calculating..."
+	#disables button click until script finishes
+	$button_ClickMe.Enabled = $false
 
-    $label_PickRandom.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("White"))
-    $label_BanRandom.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("White"))
-    $label_PickRandom.Text = ""
-    $label_BanRandom.Text = ""
+	#Resets the boxes after click
+	$label_PickRandom.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("White"))
+	$label_BanRandom.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("White"))
+	$label_PickRandom.Text = ""
+	$label_BanRandom.Text = ""
 
-    $label_modifier.BackColor = [System.Drawing.Color]::FromArgb(0,[System.Drawing.Color]::FromName("White"))
-    $label_ModifierRandom.BackColor = [System.Drawing.Color]::FromArgb(0,[System.Drawing.Color]::FromName("White"))
-    $label_modifier.Text = ""
-    $label_ModifierRandom.Text = ""
+	$label_modifier.BackColor = [System.Drawing.Color]::FromArgb(0,[System.Drawing.Color]::FromName("White"))
+	$label_ModifierRandom.BackColor = [System.Drawing.Color]::FromArgb(0,[System.Drawing.Color]::FromName("White"))
+	$label_modifier.Text = ""
+	$label_ModifierRandom.Text = ""
+	#End Reset operation
 
-    $label_PickRandom.BackColor = [System.Drawing.Color]::FromArgb(100,[System.Drawing.Color]::FromName("Yellow"))
+#----------------------------------------------------------------------------------------------------------------
 
-    $pickArray = @(
-        'Bandle City',
+#ARRAYS
+#This is where you can add your own options.
+#Note - Picks and Bans both draw from the selectionArray and regionsArray.
+
+	$regionsArray = @(
+	    
+		'Bandle City',
         'Bilgewater',
         'Demacia',
         'Ionia',
@@ -149,9 +206,14 @@ $button_ClickMe = New-Object System.Windows.Forms.Button
         'Shadow Isles',
         'Shurima',
         'Targon',
-        'Winter is coming - Freljord',
+        'Freljord',
         'Void',
-        'Zaun',
+        'Zaun'
+	
+	)
+
+    $selectionArray = @(
+
         'Vastayan'
         'Support',
         'Top',
@@ -175,122 +237,121 @@ $button_ClickMe = New-Object System.Windows.Forms.Button
         'Blonde-haired',
         'Elemental using',
         'Boomer',
-        'Zoomer'
+        'Zoomer',
+		'Invisible',
+		'Robot'
+		
     )
 
-     $banArray = @(
-        'Bandle City',
-        'Bilgewater',
-        'Demacia',
-        'Ionia',
-        'Ixtal',
-        'Noxus',
-        'Piltover',
-        'Shadow Isles',
-        'Shurima',
-        'Targon',
-        'Winter is over - Freljord',
-        'Void',
-        'Zaun',
-        'Vastayan',
-        'Support',
-        'Top',
-        'Mid',
-        'ADC',
-        'Jungle',
-        '4-legged',
-        'Female',
-        'Male',
-        'Creature',
-        'Hard-CC',
-        'KDA',
-        'Thirst-Trap',
-        'Ninja',
-        'Tech-wielder',
-        'Astral/Cosmic',
-        'Sword',
-        'Gun-wielder',
-        'Star-Guardians'
-        'Elemental using',
-        'Boomer',
-        'Zoomer'
-    )
 
     $modifierArray = @(
     
-    'All Smite',
-    'All Teleport',
-    'All Ghost',
-    'No Flash',
-    'No Skin Exceptions',
-    'Must Invade at start',
-    'Get first drake or FF at 15',
-    'AP items only',
-    'AD items only',
-    'Baron on spawn',
-    'Earn 50 kills to win',
-    'No Jungle',
-    '1 kill - 1 upgrade point'
+		'All Smite',
+		'All Teleport',
+		'All Ghost',
+		'No Flash',
+		'No Skin Exceptions',
+		'Must Invade at start',
+		'Get first drake or FF at 15',
+		'AP items only',
+		'AD items only',
+		'Baron on spawn',
+		'Earn 50 kills to win',
+		'No Jungle',
+		'1 kill - 1 upgrade point'
     
     )
-    
-    $pickArrayCount = ($pickArray.Count)
-    $banArrayCount = ($banArray.Count)
-
-    Foreach($pick in $pickArray){
+ 
+#---------------------------------------------------------------------------------------------------------------- 
+ 
+ #Start PICK Operation
+ 
+      if($objTypeRegionCheckbox.Checked -eq $true){
+			$resultantArray = $regionsArray + $selectionArray
+	}
+    elseif($objTypeRegionCheckbox.Checked -eq $false){
+		$resultantArray = $selectionArray
+	}
+	
+	
+	$label_PickRandom.BackColor = [System.Drawing.Color]::FromArgb(100,[System.Drawing.Color]::FromName("Yellow"))
+    Foreach($pick in $resultantArray){
     $label_PickRandom.Text = "$pick"
     start-sleep -Milliseconds 75
-    }
-    
-    $pickChoice = Get-Random -InputObject $pickArray
-    $label_PickRandom.Text = "$pickChoice Champions"
+    }  
+	
+	
+	$pickChoice = Get-Random -InputObject $resultantArray
+	$label_PickRandom.Text = "$pickChoice Champions"
     $label_PickRandom.BackColor = [System.Drawing.Color]::FromArgb(200,[System.Drawing.Color]::FromName("Green"))
 
+#----------------------------------------------------------------------------------------------------------------
+
+#Start BAN Operation
+
     $label_BanRandom.BackColor = [System.Drawing.Color]::FromArgb(100,[System.Drawing.Color]::FromName("Yellow"))
-
-
-
-
-    Foreach($ban in $banArray){
+    Foreach($ban in $resultantArray){
     $label_BanRandom.Text = "$ban"
     start-sleep -Milliseconds 75
-    }
+    }  
 
-     $banChoice = Get-Random -InputObject $banArray
+	$banChoice = Get-Random -InputObject $resultantArray
+   
+	
     $label_BanRandom.Text = "$banChoice Champions"
     $label_BanRandom.BackColor = [System.Drawing.Color]::FromArgb(200,[System.Drawing.Color]::FromName("Green"))
- 
 
-    $modifierChance = Get-Random -minimum 1 -Maximum 4
+#----------------------------------------------------------------------------------------------------------------
+
+#Modifier Chance Parse + Randomization
+
+	$modifierChanceVariable = 0
+	$percentageModifierChance = $ChanceDropDownBox.SelectedItem.ToString()
+	
+	if($percentageModifierChance -like '25*'){
+		$modifierChanceVariable = 5
+	}
+	elseif($percentageModifierChance -like '*50*'){
+		$modifierChanceVariable = 3
+	}
+	elseif($percentageModifierChance -like '100*'){
+		$modifierChanceVariable = 2
+	}
+	elseif($percentageModifierChance -eq $null){
+		$modifierChanceVariable = 5
+	}
+
+	
+    $modifierChance = Get-Random -minimum 1 -Maximum $modifierChanceVariable
     If($objTypeCheckbox.Checked -eq $true -and $modifierChance -eq 1){
     
-    $label_modifier.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("White"))
-    $label_modifier.Text = "Difficulty Modifier"
-    $label_ModifierRandom.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("Yellow"))
+		$label_modifier.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("White"))
+		$label_modifier.Text = "Difficulty Modifier"
+		$label_ModifierRandom.BackColor = [System.Drawing.Color]::FromArgb(60,[System.Drawing.Color]::FromName("Yellow"))
 
-    Foreach($modifier in $modifierArray){
-    $label_ModifierRandom.Text = "$modifier"
-    start-sleep -Milliseconds 300
+		Foreach($modifier in $modifierArray){
+			$label_ModifierRandom.Text = "$modifier"
+			start-sleep -Milliseconds 300
+		}
+
+
+		$label_ModifierRandom.Text = ""
+		$modifierChoice = Get-Random -InputObject $modifierArray
+		$label_ModifierRandom.Text = "$modifierChoice"
+		$label_ModifierRandom.BackColor = [System.Drawing.Color]::FromArgb(200,[System.Drawing.Color]::FromName("Green"))
+
     }
 
-
-    $label_ModifierRandom.Text = ""
-    $modifierChoice = Get-Random -InputObject $modifierArray
-    $label_ModifierRandom.Text = "$modifierChoice"
-    $label_ModifierRandom.BackColor = [System.Drawing.Color]::FromArgb(200,[System.Drawing.Color]::FromName("Green"))
-
-    }
-
-
-
+#Button Reset
+	clear-variable -name resultantArray
     start-sleep -s 2
     $button_ClickMe.Text = "Put me in again, coach"
     $button_ClickMe.Enabled = $true
     
     })
-
+#Close Click
     $Form_Picker.Controls.Add($button_ClickMe)
-
+#End Form
 
 $Form_Picker.Add_Shown({$form_Picker.Activate()})
 [void] $Form_Picker.ShowDialog()
